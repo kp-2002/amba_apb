0480cc110e21b16d6318e095cbe7fc12a684e197
kp-2002 <kaivalyaep22@gmail.com>
2024-08-19 12:51:23 +0530

    Update apb_slave.v

--- rtl/apb_slave.v
+++ rtl/apb_slave.v
@@ -9,9 +9,43 @@ module apb_slave(
   output                   pready,
   output [DATA_LENGTH-1:0] prdata);
 
-  reg [1:0] state, next_state;
-  reg [DATA_LENGTH-1:0] mem [0:ADDR_LENGTH-1];
+  parameter IDLE = 2'b00, SETUP = 2'b01, ACCESS = 2'b10;
 
-  
+  reg [1:0] state, next_state;
+
+  always @(posedge pclk) begin
+    if(~prst_n)
+      state <= IDLE;
+    else
+      state <= next_state;
+  end
+
+  always @(*) begin
+    case(state) 
+      IDLE:   next_state = (psel)? IDLE: SETUP;
+      SETUP:  next_state = ACCESS;
+      ACCESS: next_state = (pready)? ((psel)? SETUP: IDLE): ACCESS; 
+    endcase
+  end
+
+  reg [DATA_WIDTH-1:0] mem [0:2**ADDR_WIDTH];
+
+  always @(posedge pclk) begin
+    if(~prst_n) begin
+			for(int i=0; i<2**ADDR_WIDTH; i++)
+				mem[i] <= 0;
+      pready <= 1'b0;
+      prdata <= 32'b0;
+    end
+    else if(state == ACCESS) begin
+      pready <= 1'b1; 
+      if(pwrite) begin
+        mem[paddr] <= pwdata;
+      end
+      else begin
+        prdata     <= mem[paddr];
+      end
+    end
+  end
   
 endmodule
