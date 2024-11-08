e4b2de955a270fc70cf236495ecfcecda1c6c002
kp-2002 <kaivalyaep22@gmail.com>
2024-11-09 01:07:23 +0530

    Update apb_slave.v

--- rtl/apb_slave/apb_slave.v
+++ rtl/apb_slave/apb_slave.v
@@ -1,3 +1,23 @@
+//Author: Kaivalyae Panicker <kaivalyaep22@gmail.com>
+//Date  : 09.11.2024
+//Description: This is a memory module with an apb slave interface
+
+/*
+ * Copyright 2024 Kaivalyae Panicker
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+
+ *  http://www.apache.org/licenses/LICENSE-2.0
+
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 module apb_slave #(parameter ADDR_WIDTH = 30,
                    parameter DATA_WIDTH = 32,
                    parameter STRB_WIDTH = DATA_WIDTH/8) (
