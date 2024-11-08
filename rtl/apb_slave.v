c7ebf8b628c789321cb2566e33fba49686f0d27e
kp-2002 <kaivalyaep22@gmail.com>
2024-11-08 13:44:21 +0530

    Update apb_slave.v

--- rtl/apb_slave.v
+++ rtl/apb_slave.v
@@ -1,61 +1,85 @@
-module apb_slave(
-  input                        pclk,
-  input                        prst_n,
-  input                        psel,
-  input                        pwrite,
-  input                        penable,
-  input      [ADDR_LENGTH-1:0] paddr,
-  input      [DATA_LENGTH-1:0] pwdata,
-  output reg                   pready,
-  output reg [DATA_LENGTH-1:0] prdata);
+module apb_slave #(parameter ADDR_WIDTH = 30,
+                   parameter DATA_WIDTH = 32,
+                   parameter STRB_WIDTH = DATA_WIDTH/8) (
+    input                       pclk,
+    input                       pwakeup,
+    input                       prst_n,
 
-  parameter IDLE = 2'b00, SETUP = 2'b01, ACCESS = 2'b10;
+    input      [ADDR_WIDTH-1:0] paddr,
+    input               [3-1:0] pprot,
+    input                       psel,
+    input                       penable, 
+    input                       pwrite,
+    input      [DATA_WIDTH-1:0] pwdata,
+    input      [STRB_WIDTH-1:0] pstrb,
 
-  reg [1:0] state, next_state;
+    output reg                  pready,
+    output reg [DATA_WIDTH-1:0] prdata,
+    output reg                  pslverr);
 
-  always @(posedge pclk) begin
-    if(~prst_n)
-      state <= IDLE;
-    else
-      state <= next_state;
-  end
+    reg enable_latch;
+    always @(pclk or pwakeup)
+        if(~pclk)
+            enable_latch <= pwakeup;
 
-  always @(*) begin
-    case(state) 
-      IDLE:   next_state = (psel)? IDLE: SETUP;
-      SETUP:  next_state = ACCESS;
-      ACCESS: next_state = (pready)? ((psel)? SETUP: IDLE): ACCESS; 
-    endcase
-  end
+    wire   g_clk;
+    assign g_clk = (pclk & enable_latch);
 
-  reg [DATA_WIDTH-1:0] mem [0:2**ADDR_WIDTH];
+    parameter IDLE = 2'b00, WRITE = 2'b01, READ = 2'b10;
 
-  always @(posedge pclk) begin
-    if(~prst_n) begin
-      for(int i=0; i<2**ADDR_WIDTH; i++)
-        mem[i] <= 0;
-      pready <= 1'b0;
-      prdata <= 32'b0;
+    reg [1:0] state, next_state;
+
+    reg [DATA_WIDTH-1:0] mem [0:(2**ADDR_WIDTH)-1];
+
+    always @(posedge g_clk) begin
+        if(~prst_n) begin
+            state <= IDLE;
+            for(int i=0; i<2**ADDR_WIDTH; i=i+1)
+				mem[i] <= {DATA_WIDTH{1'b0}};
+            pready <= 1'b0;
+            prdata <= {DATA_WIDTH{1'b0}};
+        end
+        else
+            state <= next_state;
     end
-    else begin
-      case(state)
-        IDLE:   begin
-          pready <= 1'b0;
-        end
-        SETUP:  begin
-          pready <= 1'b0;
-        end
-        ACCESS: begin
-          pready <= 1'b1; 
-          if(pwrite) begin
-            mem[paddr] <= pwdata;
-          end
-          else begin
-            prdata     <= mem[paddr];
-          end
-        end
-      endcase
+
+    always @(*) begin
+        case(state) 
+            IDLE:    next_state = (psel & pwrite)? WRITE: ((psel & ~pwrite)? READ: IDLE);
+            WRITE:   next_state = (psel & penable)? IDLE: WRITE;
+            READ:    next_state = (psel & penable)? IDLE: READ; 
+            default: next_state = IDLE;
+        endcase
     end
-  end
-  
+
+    always @(*) begin
+        pslverr = 1'b0;
+        if(prst_n & pwakeup) begin
+            case(state)
+                IDLE:  begin
+                    pready = 1'b0;
+                    prdata = 32'b0;
+                end
+                WRITE: begin
+                    if(psel & penable) begin
+                        pready = 1'b1;
+                        for(int i=0; i<STRB_WIDTH; i=i+1)
+                            if(pstrb[i])
+                                mem[paddr][8*i +: 8] = pwdata[8*i +: 8];
+                    end
+                end
+                READ:  begin
+                    if(psel & penable) begin
+                        pready = 1'b1; 
+                        prdata     = mem[paddr];
+                    end
+                end
+                default: begin
+                    pready = 1'b0;
+                    prdata = {DATA_WIDTH{1'b0}};
+                end
+            endcase        
+        end
+    end
+
 endmodule
